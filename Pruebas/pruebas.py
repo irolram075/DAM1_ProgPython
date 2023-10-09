9b401a0bb1e282ea382ea810228dbb27c96e9a2a
irolram075 <irolram075@g.educaand.es>
2023-10-09 21:22:48 +0200

    Primeras pruebas

---
+++ Pruebas/pruebas.py
@@ -0,0 +1,21 @@
+
+num1 = int(input("Dame un número: "))
+
+num2 = int(input("Dame otro número: "))
+5
+if (num1 > num2):
+    print(num1, "es mayor que", num2)
+else:
+    if (num1 == num2):
+        print("Los números son iguales")
+    else:
+        print(num2, "es mayor que", num1)
+
+print("Adios...")
+
+
+    
+
+
+
+
