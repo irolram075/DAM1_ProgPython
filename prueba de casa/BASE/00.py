c76c939d9e11f65fc0b2c347032123fa49aeeacc
irolram075 <irolram075@g.educaand.es>
2023-10-17 00:52:49 +0200

    pruebas de casa a git

---
+++ prueba de casa/BASE/00.py
@@ -0,0 +1,7 @@
+# Hola mundo
+#Consultar tipo de dato
+print(type("hola mundo")) #tipo 'str'
+print(type(5)) #tipo 'int'
+print(type(1.5)) #tipo 'float'
+print(type(True)) #tipo 'bool'
+print(type(4 + 2j)) #tipo 'complex'
