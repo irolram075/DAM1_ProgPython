542ae07455d6263e3a4ee1892c8c52e6b3b00936
irolram075 <irolram075@g.educaand.es>
2023-10-23 17:58:14 +0200

    lm

--- EjerciciosU1/ejercicio5.py
+++ EjerciciosU1/ejercicio5.py
@@ -2,4 +2,10 @@
 aplicar y calcule e imprima por pantalla el precio final del artículo.
 """
 
-
+num1 = input("Precio primer producto: ")
+print(num1)
+num2 = input("Precio segundo producto: ") 
+print(num2)
+iva = 0.21
+suma = num1 + num2
+print("La suma de " + num1 + " + " + num2 + " es igual a " + suma)
---
+++ Pruebas/ex1.py
@@ -0,0 +1,23 @@
+"""
+Escribe un algoritmo en pseudocódigo que lea un número y te diga si es par o impar.
+El número no puede ser negativo ni mayor de 10, en tal caso solo mostrará un mensaje de error.
+"""
+
+"""
+Inicio
+    Escribe "Dame un número entre 0 y 10: "
+    Leer num
+
+    Mientras (num < 0, o > 10) hacer 
+        Escribe " Error, dame un número entre 0 y 10 "
+
+    Si (num / 2  ) entonces 
+        Escribe "num es un número par "
+
+    Sino
+        Escribe "num es un número impar "   
+
+
+
+Fin
+"""
